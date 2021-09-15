eab4aceb56a086bc4e199eb2ce6f17262d5af2fb
SantraMosambi <71275530+SantraMosambi@users.noreply.github.com>
2021-09-15 12:52:57 +0530

    Legacy commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-import './App.css';
-import Sidebar from './components/Sidebar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Glassware from './pages/Glassware';
-import Instrument from './pages/Instrument';
+import Glassware from "./pages/Glassware";
+import Instrument from "./pages/Instrument";
 
-import { Quotation, Organic, Inorganic, Indicator } from './pages/Quotation';
-import { Chemical,Msc } from './pages/Chemical';
- 
+// import { Quotation, Organic, Inorganic, Indicator } from "./pages/Quotation";
 
 function App() {
   return (
-     <Router>
+    <Router>
       <Sidebar />
-       <Switch>
-      
-        <Route path='/instrument' exact component={Instrument} />
-       
-        <Route path='/glassware' exact component={Glassware} />
-        <Route path='/quotation' exact component={Quotation} />
-        <Route path='/quotation/Organic' exact component={Organic} />
-        <Route path='/quotation/Inorganic' exact component={Inorganic} />
-        <Route path='/quotation/Indicator' exact component={Indicator} />
-{/*       
-        <Route path='/chemical/organic' exact component={Organic} />
-        <Route path='/chemical/Inorganic' exact component={Inorganic} />
-        
-        <Route path='/chemical/Indicator' exact component={Indicator} /> */}
-      <Route path='/chemical/Msc' exact component={Msc} />
-     
-      </Switch> 
+      <Switch>
+        <Route path="/instrument" exact component={Instrument} />
+
+        <Route path="/glassware" exact component={Glassware} />
+
+        {/* <Route path="/quotation" exact component={Quotation} />
+        <Route path="/quotation/Organic" exact component={Organic} />
+        <Route path="/quotation/Inorganic" exact component={Inorganic} />
+        <Route path="/quotation/Indicator" exact component={Indicator} /> */}
+      </Switch>
     </Router>
   );
 }
--- src/pages/Chemical.jsx
+++ src/pages/Chemical.jsx
@@ -1,43 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-export const chemical = () => {
-    return (
-        <div>
-            <h1>chem bond</h1>
-        </div>
-    )
-}
+export const Chemical = () => {
+  return (
+    <div className='quotation'>
+      <h1>Reports</h1>
+    </div>
+  );
+};
+
+export const Ocrganic = () => {
+  return (
+    <div className='quotation'>
+      <h1>Reports/organic</h1>
+    </div>
+  );
+};
+
+export const Icnorganic = () => {
+  return (
+    <div className='quotation'>
+      <h1>Reports/inorganic</h1>
+    </div>
+  );
+};
+
+export const Icndicator = () => {
+  return (
+    <div className='quotation'>
+      <h1>Reports/indicator</h1>
+    </div>
+  );
+};
 
 
 
-export const Organic = () => {
-    return (
-      <div className='quotation'>
-        <h1>Reports/organic</h1>
-      </div>
-    );
-  };
-  
-  export const Inorganic = () => {
-    return (
-      <div className='quotation'>
-        <h1>Reports/inOrganic</h1>
-      </div>
-    );
-  };
-  
-  export const Msc = () => {
-    return (
-      <div className='quotation'>
-        <h1>Reports/Msc</h1>
-      </div>
-    );
-  };
-  
-  export const Indicator = () => {
-    return (
-      <div className='quotation'>
-        <h1>Reports/Indicators</h1>
-      </div>
-    );
-  };
